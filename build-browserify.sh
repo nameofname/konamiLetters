5a1da002ea4c90adbd6cbc2a17f6be0d1741a7cc
nameofname <nameofemail@gmail.com>
2017-09-10 16:07:23 -0400

    got rid of webpack, browserify

--- build-browserify.sh
+++
@@ -1,6 +0,0 @@
-babel src -d lib;
-browserify -s konamiLetters lib/konamiLettersEntry.js > lib/konamiLetters.js;
-echo "window.konamiLetters = require('./lib/konamiLetters');" > tmp-browserVersion.js;
-browserify tmp-browserVersion.js > dist/browserVersion.js;
-rm tmp-browserVersion.js;
-mv lib/konamiLetters.js dist/
