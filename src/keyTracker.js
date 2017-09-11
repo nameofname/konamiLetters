119e50c619296fc292f0ee616ba7f89db8508c0f
nameofname <nameofemail@gmail.com>
2017-09-11 01:07:24 -0400

    coming up with new algorithm, not quite finished but concept is there

--- src/keyTracker.js
+++ src/keyTracker.js
@@ -5,8 +5,9 @@ const defaultSeries = [38,38,40,40,37,39,37,39,66,65];
 
 // find the matching segment to the character array at the end of the current array
 // eg. if your character array is 1, 2, 3, and your current array is 1, 1, 2, you want to match on 1, 2
-const findMatchingSegment = (charArr, currArr) => {
+const findMatchingSegment_bak = (charArr, currArr) => {
 
+    // note * this is actually impossible - the first letters must match if we are in this function
     while (currArr[0] !== charArr[0] && currArr.length) {
         currArr.shift();
     }
@@ -16,6 +17,7 @@ const findMatchingSegment = (charArr, currArr) => {
     }
 
 
+    // TODO ! this is not necessary - we check every time a new entry is placed on the end so mismatchIdx will always be he last character.
     let mismatchIdx;
     for (let i = 0; i < currArr.length; i++) {
         if (currArr[i] !== charArr[i]) {
@@ -23,25 +25,89 @@ const findMatchingSegment = (charArr, currArr) => {
         }
     }
 
+    if (mismatchIdx === undefined) {
+        return currArr;
+    }
+
+    // this did not cover all cases:
     if (mismatchIdx !== undefined) {
         const spliced = currArr.splice(mismatchIdx, currArr.length);
         return findMatchingSegment(charArr, spliced);
     } else {
         return currArr;
     }
+
+
+
+};
+
+
+/**
+ * in the case where you have a mismatch, part of what was typed might still match the beginning of the character
+ * array. for example consider :
+ * charArr = [a, b, a, b, c];
+ * currArr = [a, b, a, b, a];
+ * in this case you can still keep [a, b, a] from the end of currArr, but you have to throw out [a, b] from the front
+ *
+ * Uses  recursion, take the case of :
+ * charArr = [one me on]
+ * currArr = [one me one]
+ *
+ * I would work backwards matching the e from "one" to the e from "me" - but that doesn't fit, i need to fall back again
+ * until i hit the first e, from "one" 
+ * @param charArr
+ * @param currArr
+ * @param currI
+ * @returns {*}
+ */
+const findMatchingSegment = (charArr, currArr, currI) => {
+
+    const lastIdx = currArr.length - 1;
+    currI = (typeof currI === 'number') ? currI : lastIdx;
+    let highestMatchingIdx;
+    while (currI > 0) {
+        if (charArr[currI] === currArr[lastIdx]) {
+            highestMatchingIdx = currI;
+            currI = 0;
+        }
+        currI--;
+    }
+
+    if (!highestMatchingIdx) {
+        return [];
+    }
+
+    let noViolations = true;
+    let j = highestMatchingIdx;
+    let k = lastIdx;
+    while (j > 0) {
+        if (charArr[j] !== currArr[k]) {
+            j = 0;
+            noViolations = false;
+        }
+        j--;
+    }
+
+    if (noViolations) {
+        return currArr.splice(lastIdx - highestMatchingIdx, lastIdx);
+    } else {
+        return findMatchingSegment(charArr, currArr, lastIdx - 1);
+    }
+
 };
 
 module.exports = (charArr, callback) => {
     let currArr = [];
     charArr = charArr || defaultSeries;
 
-    document.addEventListener('keypress', (e) => {
+    document.addEventListener('keydown', (e) => {
         currArr.push(e.which);
 
         if (e.which !== charArr[currArr.length - 1]) {
             currArr = findMatchingSegment(charArr, currArr);
         }
 
+        console.log('currArr', currArr);
         if (currArr.length === charArr.length) {
             if (callback && (typeof callback === 'function')) {
                 callback();
